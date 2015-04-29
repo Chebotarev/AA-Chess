a7de1e5179a51ed265178f06cf9c8a39d0484b73
App Academy Student <student@appacademy.io>
2015-04-28 18:04:36 -0700

    Implemented Board, Game and Pieces without win conditions

--- chess/game.rb
+++ chess/game.rb
@@ -1,4 +1,4 @@
-require_relative 'board.rb'
+require_relative 'board'
 
 class Game
   attr_accessor :board
@@ -6,9 +6,29 @@ class Game
   def initialize
     @board = Board.new
   end
+
+  def play
+    while true
+      
+
+      begin
+        board.render
+        puts "Enter your start position"
+        start_pos = gets.chomp.split.map(&:to_i)
+        puts "Enter your end position"
+        end_pos = gets.chomp.split.map(&:to_i)
+
+        @board.move(start_pos, end_pos)
+      rescue
+        puts "Error!"
+
+      end
+    end
+  end
+
 end
 
 if __FILE__ == $PROGRAM_NAME
   game = Game.new
-  game.board.render
+  game.play
 end
---
+++ chess/pieces/rook.rb
@@ -0,0 +1,12 @@
+class Rook < SlidingPiece
+  def symbol
+    "R"
+  end
+
+  def move_dirs
+    [[1, 0],
+     [0, 1],
+     [-1, 0],
+     [0, -1]]
+  end
+end
